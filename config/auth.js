e3168effcca32ee51514b8c91e97029f7b69d60d
RAVI KUMAR NONIA <33859950+ravi00007@users.noreply.github.com>
2020-07-17 18:37:35 +0530

    Create auth.js

---
+++ config/auth.js
@@ -0,0 +1,10 @@
+module.exports = {
+    ensureAuthenticated: function(req, res, next) {
+      if (req.isAuthenticated()) {
+        return next();
+      }
+      req.flash('error_msg', 'Please log in to view Dashboard');
+      res.redirect('/users/login');
+    },
+   
+  };
